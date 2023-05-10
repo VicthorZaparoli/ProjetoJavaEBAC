5d4781eabea05e61d35947595b77f0715b55d9cf
Victor Zaparoli <victor.brazao@outlook.com>
2023-05-10 19:43:19 -0300

    Subindo projeto.

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Victo
+ *
+ */
+module PrimeiroProjetoEBAC {
+}
---
+++ src/packge/HelloWorld.java
@@ -0,0 +1,9 @@
+package packge;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String name = args[0];
+		System.out.print("Hellow" + " " + name + "!");
+	}
+}
